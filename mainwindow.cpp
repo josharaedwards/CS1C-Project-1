45d44b81db0d918c62300e7822af53823f4b1a4c
Junwei Chen <jwchen322@gmail.com>
2021-02-17 15:21:13 -0800

    Display contents according to button clicks

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    this->displayHome();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +15,26 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::displayAdmin()
+{
+    this->ui->adminBtn->setStyleSheet("border:none;font: 75 12pt \"Consolas\";background-color: rgb(236, 239, 244);color: rgb(46, 52, 64);");
+    this->ui->homeBtn->setStyleSheet("border:none;font: 75 12pt \"Consolas\";background-color: rgb(46, 52, 64);color: rgb(236, 239, 244);");
+    this->ui->sortBtn->setStyleSheet("border:none;font: 75 12pt \"Consolas\";background-color: rgb(46, 52, 64);color: rgb(236, 239, 244);");
+    this->ui->adminContent->raise();
+}
+
+void MainWindow::displayHome()
+{
+    this->ui->adminBtn->setStyleSheet("border:none;font: 75 12pt \"Consolas\";background-color: rgb(46, 52, 64);color: rgb(236, 239, 244);");
+    this->ui->homeBtn->setStyleSheet("border:none;font: 75 12pt \"Consolas\";background-color: rgb(236, 239, 244);color: rgb(46, 52, 64);");
+    this->ui->sortBtn->setStyleSheet("border:none;font: 75 12pt \"Consolas\";background-color: rgb(46, 52, 64);color: rgb(236, 239, 244);");
+    this->ui->homeContent->raise();
+}
+
+void MainWindow::displaySort()
+{
+    this->ui->adminBtn->setStyleSheet("border:none;font: 75 12pt \"Consolas\";background-color: rgb(46, 52, 64);color: rgb(236, 239, 244);");
+    this->ui->homeBtn->setStyleSheet("border:none;font: 75 12pt \"Consolas\";background-color: rgb(46, 52, 64);color: rgb(236, 239, 244);");
+    this->ui->sortBtn->setStyleSheet("border:none;font: 75 12pt \"Consolas\";background-color: rgb(236, 239, 244);color: rgb(46, 52, 64);");
+    this->ui->sortContent->raise();
+}
