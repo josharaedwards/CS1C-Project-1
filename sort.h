c3b33058bb96acfb5fbddc1295e83a446a9f04dc
josharaedwards <64117177+josharaedwards@users.noreply.github.com>
2021-03-06 09:04:26 -0800

    Cleaned up Sort.h

--- sort.h
+++ sort.h
@@ -6,22 +6,16 @@
 //Enum for the different sorting types
 enum SortType{team, stadium, seating, location, league};
 
-//void sort(Football footballTeams[], int numOfEntries, SortType sortBy);
 void sort(QVector<Football> footballTeams, int numOfEntries, SortType sortBy);
 
-//void sortByTeam(Football footballTeams[], int numOfEntries);
 void sortByTeam(QVector<Football> footballTeams, int numOfEntries);
 
-//void sortByStadium(Football footballTeams[], int numOfEntries);
 void sortByStadium(QVector<Football> footballTeams, int numOfEntries);
 
-//void sortBySeating(Football footballTeams[], int numOfEntries);
 void sortBySeating(QVector<Football> footballTeams, int numOfEntries);
 
-//void sortByLocation(Football footballTeams[], int numOfEntries);
 void sortByLocation(QVector<Football> footballTeams, int numOfEntries);
 
-//void sortByLeague(Football footballTeams[], int numOfEntries);
 void sortByLeague(QVector<Football> footballTeams, int numOfEntries);
 
 bool isGreaterThan(QString first, QString second);
@@ -31,6 +25,7 @@ bool isGreaterThan(int first, int second);
 int totalCapacity(QVector<Football> footballTeams, int numOfEntries);
 
 void SortRoofType(QVector<Football> footballTeams, int numOfEntries);
+
 #endif // SORT_H
 
 
