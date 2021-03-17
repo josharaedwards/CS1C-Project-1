71bf4093e5aaeadc550f2c635753637392dde248
Dayana Pulido <andpa0520@gmail.com>
2021-03-17 13:21:26 -0700

    Update init.h

--- init.h
+++ init.h
@@ -11,10 +11,8 @@
 ///@param Filename
 ///@return An array named footballTeam with the data from the file
 QVector<Football> fileRead(QString fileName);
-
 ///@brief updates Football information
 Football processLine(Football tempTeam, QString line);
-
 ///@brief updates Football information acoording to the sort request
 Football updateFootballInfo(Football& tempTeam, QStringList list, int num);
 
